4842c52c4de8d539db77d574029c8e874b65eb1a
AgDecode <82970120+AgDecode@users.noreply.github.com>
2022-04-22 00:15:26 +0300

    Add files via upload

---
+++ map/suggest.js
@@ -0,0 +1,6 @@
+ymaps.ready(init);
+
+function init() {
+    // Создаем выпадающую панель с поисковыми подсказками и прикрепляем ее к HTML-элементу по его id.
+    var suggestView1 = new ymaps.SuggestView('suggest1');
+}
